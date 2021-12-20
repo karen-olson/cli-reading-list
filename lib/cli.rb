5d9d296a997a5a611aad5a86419228ed89d8ac6b
Karen Olson <karmarie@gmail.com>
2021-12-20 16:00:03 -0600

    Set up basic functionality for Cli#run

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,32 @@
 class Cli 
 
+    def initialize 
+        @input = nil
+        @reading_list = []
+    end
+
     def run
-        puts "Welcome"
+        hello
+        query_api
+        goodbye
+    end
+
+    def hello
+        puts "Welcome to My Reading List."
+    end
+
+    def user_input
+        @input = gets.chomp
+    end
+
+    def query_api
+        puts "Please enter a search term to look for books."
+        user_input
+        binding.pry
+        # api = Api.new(@input)
+    end
+
+    def goodbye 
+        puts "Thanks for using My Reading List. See you next time!"
     end
 end
