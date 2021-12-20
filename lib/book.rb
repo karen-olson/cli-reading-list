3b79dc5d245bd1d680d69e97e26e94e55e7fde2f
Karen Olson <karmarie@gmail.com>
2021-12-20 17:03:58 -0600

    Refactor class structure to store data about reading list in Book via saved attribute

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,13 @@
 class Book
-    attr_accessor :title, :authors, :publishing_company
+    attr_accessor :title, :authors, :publishing_company, :saved
 
     @@all = []
 
-    def initialize(title, authors, publishing_company)
+    def initialize(title, authors, publishing_company, saved)
         @title = title
         @authors = authors
         @publishing_company = publishing_company
+        @saved = false
         @@all << self
     end
 
