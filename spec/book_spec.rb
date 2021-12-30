adbfb9bb213bfb8f11c987dca73dae87fdca29a8
Karen Olson <karmarie@gmail.com>
2021-12-29 19:22:29 -0600

    Add pending specs

---
+++ spec/book_spec.rb
@@ -0,0 +1,15 @@
+describe true do 
+    it "is true" do 
+        expect(false).to eq(true)
+    end
+end
+
+describe Book do 
+    it "can #show_book information given its index"
+    it "can be saved"
+    it "returns a list of all book instances"
+    it "searches all book instances given a query"
+    it "returns only 5 books for each query"
+    it "returns the reading list of all saved books"
+    it "notifies a user if their reading list is empty"
+end
