13fcec0e72657b1db513fb3096c448378ff44982
Karen Olson <karmarie@gmail.com>
2021-12-29 20:11:29 -0600

    Write spec for show_book method

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -11,12 +11,16 @@ describe Book do
         )
     end
 
-    it "#show_book prints the correct list item number, title, author/s, and publisher" do 
+    it "prints the correct list item number, title, author/s, and publisher" do 
         expected_output = " \n[2]\nTitle: Unstoppable Moses\nWritten by: Tyler James Smith\nPublisher: Flatiron Books\n \n-----------------------\n"
         expect{ unstoppable_moses.show_book(1) }.to output(expected_output).to_stdout
     end
 
-    it "can be saved"
+    it "can be saved" do 
+        unstoppable_moses.save 
+        expect(unstoppable_moses.saved).to be_true
+    end
+    
     it "returns a list of all book instances"
     it "searches all book instances given a query"
     it "returns only 5 books for each query"
