22c84df2175ffb97fa54c3f9f6336bc08bf6a6f7
Karen Olson <karmarie@gmail.com>
2021-12-29 20:49:44 -0600

    Write spec for Book.all, change let to before to avoid unnecessary Book instances

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,29 +1,39 @@
 require 'pry'
 
-describe Book do 
-    let(:unstoppable_moses) do 
-        Book.new(
+RSpec.describe Book do 
+    before(:context) do 
+        @unstoppable_moses = Book.new(
             title: "Unstoppable Moses",
             authors: ["Tyler James Smith"],
             publisher: "Flatiron Books",
             saved: false,
             query: "Flatiron Books"
         )
+
+        @winger = Book.new(
+            title: "Winger",
+            authors: ["Andrew Smith"],
+            publisher: "Simon & Schuster",
+            saved: false,
+            query: "Smith"
+        )
     end
 
     it "#show_book prints the correct list item number, title, author/s, and publisher" do 
         expected_output = " \n[2]\nTitle: Unstoppable Moses\nWritten by: Tyler James Smith\nPublisher: Flatiron Books\n \n-----------------------\n"
-        expect{ unstoppable_moses.show_book(1) }.to output(expected_output).to_stdout
-    end
-
-    it "#save changes saved attribute to true and prints a message to the terminal" do 
-        unstoppable_moses.save 
-        expect(unstoppable_moses.saved).to be true
-        expect{ unstoppable_moses.save }.to output(/Saved Unstoppable Moses to reading list./).to_stdout
+        expect{ @unstoppable_moses.show_book(1) }.to output(expected_output).to_stdout
     end
     
-    it "returns a list of all book instances" do 
-        expect(Book.all).to include(unstoppable_moses)
+    it "#save changes saved attribute to true and prints a message to the terminal" do 
+        @unstoppable_moses.save
+        expect(@unstoppable_moses.saved).to be true
+        expect{ @unstoppable_moses.save }.to output(/Saved Unstoppable Moses to reading list./).to_stdout
+    end
+    
+    it ".all returns a list of all book instances" do 
+        expect(Book.all).to include(@unstoppable_moses)
+        expect(Book.all).to include(@winger)
+        expect(Book.all.length).to eq(2)
     end
 
 
